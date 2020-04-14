702a54697e97bfd8f44343f10f177932a7acd400
DouglasAzevedo <douglaaa.s@hotmail.com>
2020-04-13 21:21:15 -0300

    commit 13/04

---
+++ app/src/main/java/br/edu/unisep/todo/ui/NewTaskActivity.kt
@@ -0,0 +1,13 @@
+package br.edu.unisep.todo.ui
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import br.edu.unisep.todo.R
+
+class NewTaskActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_new_task)
+    }
+}
